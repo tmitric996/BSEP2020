665c449d2d291d6338afde43e24389eef6fd570d
tmitric996 <tmitric996@gmail.com>
2020-04-02 14:32:50 +0200

    init comit

---
+++ bsep2020/src/main/java/com/example/bsep2020/Bsep2020Application.java
@@ -0,0 +1,13 @@
+package com.example.bsep2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bsep2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bsep2020Application.class, args);
+	}
+
+}
---
+++ bsep2020/src/test/java/com/example/bsep2020/Bsep2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bsep2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bsep2020ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
