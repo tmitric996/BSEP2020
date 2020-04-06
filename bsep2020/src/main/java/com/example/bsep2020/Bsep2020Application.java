0fbbbb7260b3b80f573e2ad395649efa94e0d88c
tmitric996 <tmitric996@gmail.com>
2020-04-06 12:53:11 +0200

    @entityScan anotacija

--- bsep2020/src/main/java/com/example/bsep2020/Bsep2020Application.java
+++ bsep2020/src/main/java/com/example/bsep2020/Bsep2020Application.java
@@ -2,10 +2,12 @@ package com.example.bsep2020;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan( basePackages = {"model"} )
 public class Bsep2020Application {
 
 	public static void main(String[] args) {
