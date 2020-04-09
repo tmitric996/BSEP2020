59a0439d1771bc2cecd135ba74a3b31450aba28b
tmitric996 <tmitric996@gmail.com>
2020-04-09 21:38:32 +0200

    adminServiceImplementation

--- bsep2020/src/main/java/service/AdminService.java
+++ bsep2020/src/main/java/service/AdminService.java
@@ -1,18 +1,9 @@
 package service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import model.Admin;
-import repository.AdminRepository;
 
-@Service
-public class AdminService {
-
-	@Autowired
-	AdminRepository adminRepository;
+public interface AdminService   {
 	
-	public Admin save(Admin admin) {
-		return adminRepository.save(admin);
-	}
+	public Admin save(Admin admin);
+	
 }
