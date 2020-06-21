881abb86df88b94df5fd18c0b892d8e7bb61221c
Katarina Pantovic <katarina.kaca.pantovic@gmail.com>
2020-06-21 14:44:26 +0200

    dodata validacija u digEntityServiceImp

--- bsep2020/src/main/java/com/example/bsep2020/service/DigEntityServiceImp.java
+++ bsep2020/src/main/java/com/example/bsep2020/service/DigEntityServiceImp.java
@@ -8,18 +8,28 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.example.bsep2020.model.DigEntity;
 import com.example.bsep2020.repository.DigEntityRepository;
+import com.example.bsep2020.security.DigEntityValidator;
 
 @Service
 public class DigEntityServiceImp implements DigEntityService {
-	
+
 	@Autowired
-	DigEntityRepository digEntityRepo;
+	 DigEntityRepository digEntityRepo;
 
 	@Override
 	public DigEntity saveEntity(DigEntity digEntity) {
-		return digEntityRepo.save(digEntity);
+		if (DigEntityValidator.isStringOnlyAlphabet(digEntity.getCommonName()) &&
+				DigEntityValidator.isStringOnlyAlphabet(digEntity.getSurname()) &&
+				DigEntityValidator.isStringOnlyAlphabet(digEntity.getGivenName()) &&
+				DigEntityValidator.isStringOnlyAlphabet(digEntity.getOrganization()) &&
+				DigEntityValidator.isStringOnlyAlphabet(digEntity.getOrganizationUnitName())) {
+			
+			return digEntityRepo.save(digEntity);
+		} else {
+			throw new IllegalArgumentException("Lose popunjena polja!");
+		}
 	}
-	
+
 	@Transactional(readOnly = true)
 	public List<DigEntity> findAllEntities() {
 		return digEntityRepo.findAll();
@@ -32,10 +42,8 @@ public class DigEntityServiceImp implements DigEntityService {
 
 	@Transactional(rollbackFor = Exception.class)
 	public void deleteEntity(DigEntity digEntity) {
-	digEntityRepo.delete(digEntity);
-		
-		
-	}
+		digEntityRepo.delete(digEntity);
 
+	}
 
 }
