4655a9b09a2956746e2c27363c798eff635a8cc2
Katarina Pantovic <katarina.kaca.pantovic@gmail.com>
2020-06-21 18:18:20 +0200

    validator za certificateDate

---
+++ bsep2020/src/main/java/com/example/bsep2020/security/CertificateDataValidator.java
@@ -0,0 +1,11 @@
+package com.example.bsep2020.security;
+
+public class CertificateDataValidator {
+	
+	public static boolean isStringOnlyAlphabet(String str) 
+    { 
+        return ((str != null) 
+                && (!str.equals("")) 
+                && (str.matches("^[a-zA-Z]*$"))); 
+    } 
+}
